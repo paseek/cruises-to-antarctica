54d2fe7750c5c8c9e6f0e20945959c0245cd41ed
paseek <paseek@yahoo.com>
2023-04-17 14:17:58 +0300

    Правки наставника модалка и отступ у блока hero

--- source/js/main.js
+++ source/js/main.js
@@ -51,7 +51,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
     const form = new Form();
     window.form = form;
     form.init();
